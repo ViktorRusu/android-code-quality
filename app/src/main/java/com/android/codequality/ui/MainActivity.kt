7e0ce090636d5089b3d110772148517da1bc35a7
ViktorRusu <viktrusu@gmail.com>
2022-10-17 22:20:45 +0300

    add noncompliant detekt code for test

--- app/src/main/java/com/android/codequality/ui/MainActivity.kt
+++ app/src/main/java/com/android/codequality/ui/MainActivity.kt
@@ -3,11 +3,17 @@ package com.android.codequality.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.namespace.R
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        GlobalScope.launch {
+            delay(2000)
+        }
     }
 }
