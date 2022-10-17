db492f35e3047a7498f7df27338ca86cab687a15
ViktorRusu <viktrusu@gmail.com>
2022-10-17 23:08:30 +0300

    add noncomlian detekt code for test

--- app/src/main/java/com/android/codequality/ui/MainActivity.kt
+++ app/src/main/java/com/android/codequality/ui/MainActivity.kt
@@ -1,13 +1,30 @@
 package com.android.codequality.ui
 
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.namespace.R
 
 class MainActivity : AppCompatActivity() {
 
+    init {
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            Log.d("TAG", "Delayed message")
+        }, 2000)
+
+
+        try {
+
+        } catch (e: Exception) {
+            throw e
+        }
     }
 }
